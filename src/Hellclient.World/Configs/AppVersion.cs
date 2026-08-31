d893acccbe8e72e857e72c83c99eca3758e42cb5
Jarlyyn <jarlyyn@qq.com>
2026-08-31 14:05:32 +0800

    update

--- src/Hellclient.World/Configs/AppVersion.cs
+++ src/Hellclient.World/Configs/AppVersion.cs
@@ -6,19 +6,19 @@ public class AppVersion
 {
     public static DateVersion Version { get; set; } = new DateVersion()
     {
-        Major = 1,
+        Major = 2,
         Year = 2026,
-        Month = 5,
-        Day = 14,
+        Month = 8,
+        Day = 31,
         Patch = 0,
         Build = ""
     };
     public static DateVersion APIVersion { get; set; } = new DateVersion()
     {
-        Major = 1,
+        Major = 2,
         Year = 2026,
-        Month = 5,
-        Day = 14,
+        Month = 8,
+        Day = 31,
         Patch = 0,
         Build = ""
     };
