f4e29d7c0789f4ec5510f69c4e50ce4499e3b66a
Jarlyyn <jarlyyn@qq.com>
2026-09-17 13:13:18 +0800

    fix

--- src/Hellclient.V8Engine/Cores/V8ScriptEngine.cs
+++ src/Hellclient.V8Engine/Cores/V8ScriptEngine.cs
@@ -68,7 +68,7 @@ public class V8ScriptEngineFactory : IScriptEngineFactory
         Directory.CreateDirectory(Path.Combine(Deployment.Instance.ScriptsPath, ID, "script"));
         var data = File.ReadAllText(Path.Combine(Deployment.Instance.SystemPath, "template", "script", "v8.toml"));
         File.WriteAllText(Path.Combine(Deployment.Instance.ScriptsPath, ID, "script.toml"), data);
-        var scriptdata = File.ReadAllText(Path.Combine(Deployment.Instance.SystemPath, "template", "script", "v8.js"));
-        File.WriteAllText(Path.Combine(Deployment.Instance.ScriptsPath, ID, "script", "script.js"), scriptdata);
+        var scriptdata = File.ReadAllText(Path.Combine(Deployment.Instance.SystemPath, "template", "script", "main.v8.js"));
+        File.WriteAllText(Path.Combine(Deployment.Instance.ScriptsPath, ID, "script", "main.js"), scriptdata);
     }
 }
